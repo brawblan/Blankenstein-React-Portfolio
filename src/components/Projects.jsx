9635345afa8b511369ea8d937bcd266aef0dcacb
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2021-03-24 23:45:00 -0500

    pushing just to add to branch and delete bad comp

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,63 @@
-import { React } from 'react'
+import { React, useState, useEffect } from 'react'
 import './Projects.css'
 import TitleFlipSwitch from '../components/TitleFlipSwitch'
 import { Link } from 'react-router-dom'
 import projectsArr from '../utils/projectsArray'
 
 const Projects = () => {
+  const [page, setPage] = useState(null)
+
+  const query = `
+  {
+    projectContentCollection {
+      items {
+        id
+        title
+        route
+        repo
+        siteLink
+        projectImage
+        description
+      }
+    }
+  }
+  `
+
+  useEffect(() => {
+    window
+    .fetch(`https://graphql.contentful.com/content/v1/spaces/noefj5y57vr9/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer MTmSGOTdxvfPA7rRthSdqkZRyhmees_WMpcl_BBe9g4`,
+      },
+      body: JSON.stringify({ query }),
+    })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+        setPage(data.projectContentCollection.items);
+      });
+  }, [])
 
   return (
     <div className="project-page-container">
       <TitleFlipSwitch title='Portfolio Projects' />
       <div className="project-container">
-        {projectsArr.map(({ title, src, route }) => (
-          <Link to={`/${route}`} className="project-page-btn">
-            <div className="project-wrapper">
-              <h3>{title}</h3>
-              <img src={src} alt={title} width="300" height="150" />
-            </div>
-          </Link>
-        ))}
+        {page.map((item, index ) => {
+          if (index === 0) {
+            return (
+            <Link to={`/${item.route}`} className="project-page-btn">
+              <div className="project-wrapper">
+                <h3>{item.title}</h3>
+                <img src={item.src} alt={item.title} width="300" height="150" />
+              </div>
+            </Link>
+            )
+          }
+        })}
       </div>
     </div>
   )
--- src/pages/projectPages/TemplateTest.jsx
+++ src/pages/projectPages/TemplateTest.jsx
@@ -8,22 +8,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 
-const query = `
-{
-  projectContentCollection {
-    items {
-      title
-      route
-      repo
-      siteLink
-      projectImage
-      description
+const TemplateTest = ({ id }) => {
+  const query = `
+  {
+    projectContentCollection {
+      items {
+        id
+        title
+        route
+        repo
+        siteLink
+        projectImage
+        description
+      }
     }
   }
-}
-`
-
-const TemplateTest = () => {
+  `
+  const [page, setPage] = useState(null);
   const [isHover, setIsHover] = useState(false)
   const addHoverClass = () => {
     setIsHover(true)
@@ -32,82 +33,79 @@ const TemplateTest = () => {
     setIsHover(false)
   }
 
-  const [page, setPage] = useState(null);
-
   useEffect(() => {
-    window
-      .fetch(`https://graphql.contentful.com/content/v1/spaces/noefj5y57vr9/`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          // Authenticate the request
-          Authorization: `Bearer MTmSGOTdxvfPA7rRthSdqkZRyhmees_WMpcl_BBe9g4`,
-        },
-        // send the GraphQL query
-        body: JSON.stringify({ query }),
-      })
+    fetch(`https://graphql.contentful.com/content/v1/spaces/noefj5y57vr9/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer MTmSGOTdxvfPA7rRthSdqkZRyhmees_WMpcl_BBe9g4`,
+      },
+      body: JSON.stringify({ query }),
+    })
       .then((response) => response.json())
       .then(({ data, errors }) => {
         if (errors) {
           console.error(errors);
         }
-        
-        // rerender the entire component with new data
-        setPage(data.projectContent);
+        setPage(data.projectContentCollection.items);
       });
   }, [])
 
-  console.log(page);
-  
-  // show a loading screen case the data hasn't arrived yet
   if (!page) {
     return "Loading...";
   }
-
+      
   return (
     <>
       <NavBar />
       <div className="project-page">
 
-        <div className="project-picture-and-description">
-          <Link
-            to={'/projects'}
-            className="go-back-btn"
-            onMouseOver={() => addHoverClass()}
-            onMouseLeave={() => removeHoverClass()}
-          >
-            <FontAwesomeIcon 
-              icon={faArrowAltCircleLeft} 
-              className="left-arrow" 
-              pulse={isHover}
-            />
-            Go Back to Projects
-          </Link>
-          <img src="../../assets/landingPage.png" alt="" width="600" height="300" />
-          <GitHubBtn repo={page.repo} />
-          <div className="project-description">
-            <h3>Project Brief</h3>
-            <p>
-            {page.description}
-            </p>
-          </div>
-        </div>
+        {page.map((project, index) => {
+          if (index === 0) {
+            return (
+              <>
+                <div className="project-picture-and-description">
+                  <Link
+                    to={'/projects'}
+                    className="go-back-btn"
+                    onMouseOver={() => addHoverClass()}
+                    onMouseLeave={() => removeHoverClass()}
+                  >
+                    <FontAwesomeIcon 
+                      icon={faArrowAltCircleLeft} 
+                      className="left-arrow" 
+                      pulse={isHover}
+                    />
+                    Go Back to Projects
+                  </Link>
+                  <img src="../../assets/landingPage.png" alt="" width="600" height="300" />
+                  <GitHubBtn repo={project.repo} />
+                  <div className="project-description">
+                    <h3>Project Brief</h3>
+                    <p>
+                      {project.description}
+                    </p>
+                  </div>
+                </div>
 
-        <div className="project-link-and-tech">
-          <div className="project-link">
-            <h4>Visit Website</h4>
-            <WebsiteLinkBtn title={page.title} />
-          </div>
-          <div className="project-tech">
-            <h4>Tech Used</h4>
-            <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-            </ul>
-          </div>
-        </div>
-
+                <div className="project-link-and-tech">
+                  <div className="project-link">
+                    <h4>Visit Website</h4>
+                    <WebsiteLinkBtn title={project.title} />
+                  </div>
+                  <div className="project-tech">
+                    <h4>Tech Used</h4>
+                    <ul>
+                      <li>HTML</li>
+                      <li>CSS</li>
+                      <li>JavaScript</li>
+                    </ul>
+                  </div>
+                </div>
+              </>
+            )
+          }
+        })}
       </div>
       <Footer />
     </>
